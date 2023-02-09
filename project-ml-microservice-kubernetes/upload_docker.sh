b941f0fa735b2d86962ccae3e9285681b159a716
timothy <willytimothy01@gmail.com>
2023-02-09 13:57:17 +0100

    update docker output and kubernetes output

--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -6,10 +6,14 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath=willytimothy01/flaskapp
 
 # Step 2:  
 # Authenticate & tag
+echo "$DP" | docker login -u "willytimothy01" --password-stdin
+docker tag flaskapp $dockerpath
 echo "Docker ID and Image: $dockerpath"
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
